2b2b679b32bec2d1a6c216fa7136e40a7b48d7fd
Rashik Prajapati <rshkprjpt@gmail.com>
2026-02-20 20:38:54 +0545

    added chat model

---
+++ api/src/models/chat.model.ts
@@ -0,0 +1,32 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface ChatDocument extends Document {
+  participants: mongoose.Types.ObjectId[];
+  lastMessage: mongoose.Types.ObjectId;
+  isGroup: boolean;
+  groupName: string;
+  createdBy: mongoose.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const chatSchema = new Schema<ChatDocument>(
+  {
+    participants: [
+      {
+        types: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
+    lastMessage: { type: Schema.Types.ObjectId, ref: "Message", default: null },
+    isGroup: { type: Boolean, default: false },
+    createdBy: { type: Schema.Types.ObjectId, ref: "user", required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const chatModel = mongoose.model<ChatDocument>("Chat", chatSchema);
+export default chatModel;
